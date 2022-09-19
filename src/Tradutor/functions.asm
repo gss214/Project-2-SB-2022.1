236b3d7811586e2b5fee08e03a44a8cdaf709ffc
Guilherme Souza <gss214@hotmail.com>
2022-09-19 20:29:43 -0300

    feat(functions.asm): criada a função convert_str_2_int

--- src/Tradutor/functions.asm
+++ src/Tradutor/functions.asm
@@ -1,11 +1,21 @@
+;Projeto 2 da disciplina Software Basico
+;Universidade de Brasilia
+;Instituto de Ciencias Exatas
+;Departamento de Ciencia da Computacao
+;Software Basico 1/2021
+;Professor: Bruno Macchiavello
+;Desenvolvido por: Guilherme Silva Souza e Maria Eduarda Machado de Holanda
+
+;macro para ler uma string
 %macro read_str_macro 1
 mov eax, 3
 mov ebx, 0
-mov ecx, %1 ; ponteiro da lbl
-mov edx, 10 ; tamanho
+mov ecx, %1 
+mov edx, 10 
 int 80h
 %endmacro
 
+;macro para printar uma string
 %macro print_str_macro 2
 mov eax, 4
 mov ebx, 1 
@@ -44,7 +54,6 @@ loop_s_input:
         ; loop enquanto eax for menor que o numero de iteracoes
         cmp esi, ebx
         jl loop_s_input
-
 fim_s_input:
         mov eax, ecx ;retorna o n de bytes lidos no eax
 
@@ -69,12 +78,10 @@ s_output:
 
         sub esi, esi ; esi = 0 eh o indice
         sub ecx, ecx ; n total de bytes
-
 loop_s_output:
         mov edi, [EBP+8] ; edi = ponteiro pra label
         mov ebx, [EBP+12] ; ebx = numero de iteracoes
 
-        ;chama output
         push dword [edi+esi*4]
         call output
 
@@ -85,7 +92,6 @@ loop_s_output:
         ; loop enquanto eax for menor que o numero de iteracoes
         cmp esi, ebx
         jl loop_s_output
-
 fim_s_output:
         mov eax, ecx ;retorna o n de bytes escritos no eax
         
@@ -96,7 +102,6 @@ fim_s_output:
         pop ebx
         pop EBP 
         ret 4
-
 
 input:
         ;criando a pilha local (frame de pilha)
@@ -111,47 +116,18 @@ input:
         
         print_str_macro bytes_msg, bytes_msg_size ; exibe a msg "Foram lidos/escritos" 
 
-        ;converre o n de bytes int->str
+        ;converte o n de bytes int->str
         push dword [number_of_bytes]
         call convert_int_2_str
         mov eax, [result_int_str]
         
         print_str_macro result_int_str, [result_int_str_size] ; exibe a qnt de bytes lidos
         print_str_macro bytes_msg_2, bytes_msg_2_size ; exibe a msg " bytes"
-        
-str_2_int:
-        mov eax, 0 ; eax = valor total
-        mov ebx, number ; ebx = numero
 
-loopchar: 
-        movzx ecx, byte [ebx] ;carrega o char 
-        inc ebx
-        ;verificando o sinal (soh ignora pq vai ser tratado no final)
-        cmp ecx, '-'
-        je loopchar
-        ; verificando se chegou no fim
-        cmp ecx, '0'
-        jb ehneg?
-        cmp ecx, '9'
-        ja ehneg?
+        push number
+        call convert_str_2_int
 
-        ;convertendo
-        sub ecx, '0'
-        imul eax, 10
-        add eax, ecx
-
-        jmp loopchar
-
-ehneg?:    
-        mov ebx, [EBP+8] ; ebx = numero
-        movzx ecx, byte [ebx] ;carrega o primeiro char
-        cmp ecx, '-' ;verifica se é um -
-        jne fim_input ;se nao for pula para o fim
-        imul eax, -1 ;se for multiplica por -1
-
-fim_input:
-        mov [result_str_int], eax ;move o resultado para result_str_int
-        
+fim_input:        
         ;desempilha os registradores usado
         popa
         pop EBP
@@ -191,6 +167,49 @@ output:
         pop EBP
         
         mov eax, [number_of_bytes] ;retorna o n de bytes escritos no eax
+        ret 4
+
+; função para converter o str para int, o resultado vai pra result_str_int
+convert_str_2_int:
+        ;criando a pilha local (frame de pilha)
+        push EBP
+        mov EBP,ESP
+
+        pusha
+
+        mov eax, 0 ; eax = valor total
+        mov ebx, [EBP+8] ; ebx = numero
+loopchar: 
+        movzx ecx, byte [ebx] ;carrega o char 
+        inc ebx
+        ;verificando o sinal (soh ignora pq vai ser tratado no final)
+        cmp ecx, '-'
+        je loopchar
+        ; verificando se chegou no fim
+        cmp ecx, '0'
+        jb ehneg?
+        cmp ecx, '9'
+        ja ehneg?
+
+        ;convertendo
+        sub ecx, '0'
+        imul eax, 10
+        add eax, ecx
+
+        jmp loopchar
+ehneg?:    
+        mov ebx, [EBP+8] ; ebx = numero
+        movzx ecx, byte [ebx] ;carrega o primeiro char
+        cmp ecx, '-' ;verifica se é um -
+        jne fim_convert_str_2_int ;se nao for pula para o fim_convert_str_2_int
+        imul eax, -1 ;se for multiplica por -1
+fim_convert_str_2_int:
+        mov [result_str_int], eax ;move o resultado para result_str_int
+        
+        ;desempilha os registradores usado
+        popa
+        pop EBP
+        
         ret 4
 
 ; função para converter o int para str, o resultado vai pra result_int_str
